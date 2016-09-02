be190b406d858e2e7bc22e8dcd62217d034ba6b8
Ralph Schlosser <ralph.schlosser@gmail.com>
2016-09-02 13:17:13 +0100

    Some cleanup.

--- hadoop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Just a quick wrapper script to expose the hadoop CLI.
-# NOTE: File access from the host is relative to the shared volume set up in the docker compose file.
-
-source base/files/common.sh
-greenprint "+++ Executing hadoop command on namenode docker"
-
-docker exec namenode hadoop $@
